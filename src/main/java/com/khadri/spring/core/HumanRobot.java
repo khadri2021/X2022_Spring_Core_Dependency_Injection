989a660fa6e9dd2aa8ae1ebec8f760565848ca1a
khadri2021 <khadri.institutions@gmail.com>
2023-06-02 08:57:27 +0530

    DI and IOC

---
+++ src/main/java/com/khadri/spring/core/HumanRobot.java
@@ -0,0 +1,62 @@
+package com.khadri.spring.core;
+
+import java.util.List;
+
+public class HumanRobot {
+    String name;
+    Head head ;
+    Legs legs;
+    Hands hands;
+    List<Integer> boltsAndScrews;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Head getHead() {
+        return head;
+    }
+
+    public void setHead(Head head) {
+        this.head = head;
+    }
+
+    public Legs getLegs() {
+        return legs;
+    }
+
+    public void setLegs(Legs legs) {
+        this.legs = legs;
+    }
+
+    public Hands getHands() {
+        return hands;
+    }
+
+    public void setHands(Hands hands) {
+        this.hands = hands;
+    }
+
+    public List<Integer> getBoltsAndScrews() {
+        return boltsAndScrews;
+    }
+
+    public void setBoltsAndScrews(List<Integer> boltsAndScrews) {
+        this.boltsAndScrews = boltsAndScrews;
+    }
+
+    @Override
+    public String toString() {
+        return "HumanRobot{" +
+                "name='" + name + '\'' +
+                ", head=" + head +
+                ", legs=" + legs +
+                ", hands=" + hands +
+                ", boltsAndScrews=" + boltsAndScrews +
+                '}';
+    }
+}
---
+++ src/main/java/com/khadri/spring/core/Main.java
@@ -0,0 +1,12 @@
+package com.khadri.spring.core;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        HumanRobot humanRobot = (HumanRobot) context.getBean("robot");
+        System.out.println(humanRobot);
+    }
+}
